7f68e502e909c323d0e530d36c42740ab5d326cc
RazelRaz <razel16dcm@gmail.com>
2023-06-23 22:56:08 +0600

    first commit

---
+++ object/object.js
@@ -0,0 +1,11 @@
+let person = {
+    first_name: 'john',
+    last_name: 'doe',
+    age: 50,
+    getName: () => {
+        return person.first_name + ' ' + person.last_name;
+    },
+    city: ['Madrid', 'Dubai', 'Berlin'],
+}
+console.log(person.getName());
+console.log(person.city[1]);
